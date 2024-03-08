0263d3ffc08b1df676ef380db907b8a454fed61e
Alexandru Grecu <alexandru.grecu27@gmail.com>
2024-03-08 19:58:58 +0200

    nextui

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,18 @@
+import { Chip, Spinner } from '@nextui-org/react'
+
+function App(): JSX.Element {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-extrabold text-blue-500">Zenith Boilerplate</h1>
+      <a href="https://github.com/arounte">
+        <h2 className="text-lg font-light text-blue-400 hover:text-pink-500">GitHub: @Arounte</h2>
+      </a>
+      <Spinner />
+      <Chip color="primary" variant="shadow" className="my-2">
+        NextUI works 🍻
+      </Chip>
+    </div>
+  )
+}
+
+export default App
